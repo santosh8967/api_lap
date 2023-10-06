c5360668094e9a1df3f8dab2f2bac9dfab7c029e
santosh8967 <124495354+santosh8967@users.noreply.github.com>
2023-10-06 15:23:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-//create div
+// Create div
 let container = document.createElement("div");
-container.setAttribute("class","container");
+container.setAttribute("class", "container");
 document.body.append(container);
 
-//create paragraph
-let paragraph = document.createElement("div");
-paragraph.setAttribute("class","division")
+// Create paragraph
+let paragraph = document.createElement("p");
+paragraph.setAttribute("class", "division");
 container.append(paragraph);
 
-//function
-async function getdata()
-{
-    try{
+// Function
+async function getData() {
+    try {
         let url = await fetch("https://api.apicagent.com/?ua=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/113.0.0.0%20Safari/537.36");
         let data = await url.json();
         console.log(data);
-        var value = (data.os);
-        renderdata(value)
-    }
-    catch{
-
+        renderData(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
     }
 }
-getdata();
+getData();
 
-function renderdata(data=[])
-{
-console.log(data) 
-paragraph.innerHTML+=`<p style="font-weight:bold">OS</p>`
-paragraph.innerHTML+=`<p> Name: ${data.name}</p>`
-paragraph.innerHTML+=`<p>Platform: ${data.platform}</p>`
-paragraph.innerHTML+=`<p>Version: ${data.version}</p>`
+function renderData(data) {
+    console.log(data);
+    paragraph.innerHTML += `<p style="font-weight:bold">OS</p>`;
+    paragraph.innerHTML += `<p>Name: ${data.os.name}</p>`;
+    paragraph.innerHTML += `<p>Platform: ${data.os.platform}</p>`;
+    paragraph.innerHTML += `<p>Version: ${data.os.version}</p>`;
 }
